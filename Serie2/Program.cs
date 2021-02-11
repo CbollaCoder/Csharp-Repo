7ffe1e392d4ce4fe7289810aa4dc15723c8e11b3
America Rivas <amerivasmur@gmail.com>
2021-02-11 00:15:10 -0400

    Serie 2

---
+++ Serie2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1,2,5,7,10 .....
+
+            int n = Int32.Parse(Console.ReadLine());
+            int a = 1;
+            int add = 1;
+
+            for(int i=0; i < n; i++)
+            {
+                Console.WriteLine("{0}",a);
+                a = a + add; 
+                add = add + 2; 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
