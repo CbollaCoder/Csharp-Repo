eb5a996f1717fe4939b38089cd737d60dc96facb
America Rivas <amerivasmur@gmail.com>
2021-02-11 14:15:37 -0400

    Serie 3

---
+++ Serie3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //01001000100001....
+
+            int n = Int32.Parse(Console.ReadLine());
+            
+
+            for(int i=1; i<=n; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.WriteLine("0");
+                    if (j == i) Console.WriteLine("1");
+                }   
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
