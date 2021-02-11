90eb2c25503ebf3ff417dc2d4fbf1c8a30e00176
America Rivas <amerivasmur@gmail.com>
2021-02-10 23:12:49 -0400

    Serie Aritmetica 1

---
+++ SerieAritmetica1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieAritmetica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7,10,13....
+            int n = Convert.ToInt32(Console.ReadLine());
+            int first = 7;
+           // int item = 0;
+
+            for (int i=1; i<=n; i++)
+            {
+                Console.WriteLine("{0}", first);
+                first = first + 3;
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
